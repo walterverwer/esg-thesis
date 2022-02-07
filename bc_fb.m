acfca988b7f79004e7be28a749ca3013d574ae8e
Walter Verwer <walterverwer@gmail.com>
2022-02-07 16:26:31 +0100

    ad

--- bc_fb.m
+++
@@ -1,7 +0,0 @@
-function res = bc_fb(ya, yb, p_B, p_G)
-% Write boundary conditions in the form g(ya, yb) = 0, where ya denotes
-% V(0) and yb denotes V(1).
-res = [ya(1)- p_B
-       yb(1)- p_G];
-end
-
