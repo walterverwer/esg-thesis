184a49baab4d601298c0ef846abfa872379691ae
Walter Verwer <walterverwer@gmail.com>
2022-02-07 16:21:44 +0100

    ad

--- isOptim.m
+++
@@ -1,6 +0,0 @@
-function [position,isterminal,direction] = isOptim(z,y)
-  position      = y(2); % The value that we want to be zero
-  isterminal    = 0;  % Halt integration 
-  direction     = 0;   % The zero can be approached from either direction
-end
-
